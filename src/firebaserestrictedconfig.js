c4c3934ccd997d78ea909e67ea23453f5502b91e
Abhijith Krishna G <130480019+uselessbruh@users.noreply.github.com>
2025-08-13 16:39:45 +0530

    Update firebaserestrictedconfig.js

--- src/firebaserestrictedconfig.js
+++ src/firebaserestrictedconfig.js
@@ -4,16 +4,17 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAbxMvQBrsUBEFpLvEGy5izLdAE2XECD8w",
-    authDomain: "garden-665c8.firebaseapp.com",
-    projectId: "garden-665c8",
-    storageBucket: "garden-665c8.appspot.com",
-    messagingSenderId: "82745677755",
-    appId: "1:82745677755:web:3f0d41c9375c52784b169e"
+    apiKey: "admin-apiKey",
+    authDomain: "admin-authDomain",
+    projectId: "admin-projectId",
+    storageBucket: "admin-storageBucket",
+    messagingSenderId: "admin-messagingSenderId",
+    appId: "admin-appId"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app)
+
 export { db, collection, getDocs, auth, storage };
