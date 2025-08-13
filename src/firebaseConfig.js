1b60857597fa31d31eda9a42ae251599798fe510
Abhijith Krishna G <130480019+uselessbruh@users.noreply.github.com>
2025-08-13 16:39:12 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCf49k1-ZuV8N4parAVFF-fXJhJ9-pcieQ",
-    authDomain: "garden-67579.firebaseapp.com",
-    projectId: "garden-67579",
-    storageBucket: "garden-67579.appspot.com",
-    messagingSenderId: "333607444392",
-    appId: "1:333607444392:web:c59132938f17f25699c79d"
+    apiKey: "general-apiKey",
+    authDomain: "general-authDomain",
+    projectId: "general-projectId",
+    storageBucket: "general-storageBucket",
+    messagingSenderId: "general-messagingSenderId",
+    appId: "general-appId"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,3 +17,4 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const storage =getStorage(app)
 export { db, collection, getDocs, auth , storage};
+
